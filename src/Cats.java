42cf3209e03057ba52907bc656386aea75adc5a9
Aleksander Zhuk <Zhuchko.s@gmail.com>
2024-01-29 19:40:31 +0300

    Добавил функцию для добавлени котика и его типа

--- src/Cats.java
+++ src/Cats.java
@@ -10,6 +10,7 @@ public class Cats {
                     if (resultSet.next() && resultSet.getInt(1) == 1) {
                         type = 1;
                         System.out.println("Таблица уже существует.");
+                        conn.close();
                     }
                 }
             }
@@ -17,7 +18,8 @@ public class Cats {
             e.printStackTrace();
         }
         if (type == 0) {
-            try (Connection conn = DriverManager.getConnection("jdbc:sqlite:D:/SQL/new/My_cats.db")) {
+            try  {
+                Connection conn = DriverManager.getConnection("jdbc:sqlite:D:/SQL/new/My_cats.db");
                 String sql = "CREATE TABLE IF NOT EXISTS cats (\n"
                         + " id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,\n"
                         + " name VARCHAR(20) NOT NULL,\n"
@@ -33,10 +35,87 @@ public class Cats {
                     System.out.println("таблица создана");
                 }
             }
+
             catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
         }
+    }
+    public void insert_cat (String name, String type, int age, double weight){
+        String type_test = "";
+        ResultSet resultSet = null;
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:D:/SQL/new/My_cats.db");
+            String sql = "SELECT type FROM types WHERE type = ?";
+            conn.prepareStatement(sql);
+            PreparedStatement statement;
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, type);
+
+            resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                type_test = resultSet.getString("type");
+            }
+            conn.close();
+            statement.close();
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+            if (type_test.equals(type)) {
+                try {
+                    Connection conn = DriverManager.getConnection("jdbc:sqlite:D:/SQL/new/My_cats.db");
+                    String sql2 = "INSERT INTO cats (name, type_id, age, weight)" +
+                            "VALUES (?, (SELECT id FROM types WHERE type = ?), ?, ?)" ;
+                    conn.prepareStatement(sql2);
+                    PreparedStatement statement;
+                    statement = conn.prepareStatement(sql2);
+                    statement.setString(1, name);
+                    statement.setString(2, type);
+                    statement.setInt(3, age);
+                    statement.setDouble(4, weight);
+                    statement.executeUpdate();
+                    conn.close();
+                    statement.close();
+                    System.out.println("Котик добавлен");
+                }catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            else {
+                try {
+                    Connection conn = DriverManager.getConnection("jdbc:sqlite:D:/SQL/new/My_cats.db");
+                    String sql2 = "INSERT INTO types (type) VALUES (?)" ;
+                    conn.prepareStatement(sql2);
+                    PreparedStatement statement;
+                    statement = conn.prepareStatement(sql2);
+                    statement.setString(1, type);
+                    statement.executeUpdate();
+                    conn.close();
+                    statement.close();
+                    System.out.println("Тип котика добавлен");
+                }catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+                try {
+                    Connection conn = DriverManager.getConnection("jdbc:sqlite:D:/SQL/new/My_cats.db");
+                    String sql2 = "INSERT INTO cats (name, type_id, age, weight)" +
+                            "VALUES (?, (SELECT id FROM types WHERE type = ?), ?, ?)" ;
+                    conn.prepareStatement(sql2);
+                    PreparedStatement statement;
+                    statement = conn.prepareStatement(sql2);
+                    statement.setString(1, name);
+                    statement.setString(2, type);
+                    statement.setInt(3, age);
+                    statement.setDouble(4, weight);
+                    statement.executeUpdate();
+                    conn.close();
+                    statement.close();
+                    System.out.println("Котик добавлен");
+                }catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
     }
 }
 
--- src/CreateDatabaseCats.java
+++ src/CreateDatabaseCats.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
 public class CreateDatabaseCats {
     public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
 
         Cats cats = new Cats();
 
+
+        while (true){
+            System.out.println("Если вам надо добавить котика (Y/N)");
+            String answer = str.nextLine();
+            if(answer.equals("N")){
+                break;
+            }
+            else  if (answer.equals("Y")){
+                System.out.println("Введите name:");
+                String name = str.nextLine();
+                System.out.println("Введите type:");
+                String type = str.nextLine();
+                System.out.println("Введите age:");
+                int age = str.nextInt();
+                str.nextLine();
+                System.out.println("Введите weight:");
+                //double weight = str.nextDouble();
+
+                String input = str.nextLine();
+                if (input.contains(",")) {
+                    input = input.replace(",", ".");
+                }
+                double weight = Double.parseDouble(input);
+                cats.insert_cat(name, type, age, weight);
+            }
+        }
     }
 }
+
